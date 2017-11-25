2568975cc37ae2900c2ae7aeb23d0464944744a6
vid4r <vid4r.dergeraet@gmx.de>
2017-11-25 13:26:29 +0100

    Änderung Header

--- BullsCowGame/FBullCowGame.h
+++ BullsCowGame/FBullCowGame.h
@@ -1,10 +1,14 @@
 #pragma once
 
+
 class FBullCowgame
 {
 public:
 	void Reset(); // ToDo
 	int GetMaxTries();
+	int GetCurrentTry();
+	bool IsGameWon();
+	bool CheckGuessValitity(string);//ToDo einen besseren Rückgabewert finden
 
 
 private:
